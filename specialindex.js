23f11c3b749392f6fde2335dc3ec819ac39952ff
Umherirrender <umherirrender_de.wp@web.de>
2020-09-05 23:34:20 +0200

    build: Replace jshint/jsonlint with eslint
    Fix issues with eslint --fix
    
    Bug: T210365
    Bug: T220036
    Change-Id: I9d649268edffd193c2a73471fc760cd54aa13e5c

--- specialindex.js
+++ specialindex.js
@@ -3,29 +3,29 @@
 
 var appendCSS;
 
-appendCSS('.mw-index-hidden {'+
-	'	display:none;'+
-	'}'+
-	'ul.mw-index-expanded {'+
-	'	display:block;'+
-	'}'+
-	'span.mw-index-expanded {'+
-	'	display:inline !important;'+
-	'	visibility:visible !important;'+
-	'}'+
-	'#use-js-note {'+
-	'	display: block !important;'+
+appendCSS( '.mw-index-hidden {' +
+	'	display:none;' +
+	'}' +
+	'ul.mw-index-expanded {' +
+	'	display:block;' +
+	'}' +
+	'span.mw-index-expanded {' +
+	'	display:inline !important;' +
+	'	visibility:visible !important;' +
+	'}' +
+	'#use-js-note {' +
+	'	display: block !important;' +
 	'}'
 );
 
 /*
  * Switch details between hidden/shown
  */
-function toggleVisibility(idNumber) {
-	var openarrow = document.getElementById("mw-index-open-"+idNumber);
-	var closearrow = document.getElementById("mw-index-close-"+idNumber);
-	var inner = document.getElementById("mw-index-inner-"+idNumber);
-	if (openarrow.className === 'mw-index-expanded') {
+function toggleVisibility( idNumber ) {
+	var openarrow = document.getElementById( 'mw-index-open-' + idNumber ),
+		closearrow = document.getElementById( 'mw-index-close-' + idNumber ),
+		inner = document.getElementById( 'mw-index-inner-' + idNumber );
+	if ( openarrow.className === 'mw-index-expanded' ) {
 		openarrow.className = 'mw-index-hidden';
 		closearrow.className = 'mw-index-expanded';
 		inner.className = 'mw-index-expanded';
